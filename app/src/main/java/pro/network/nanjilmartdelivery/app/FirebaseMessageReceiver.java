918b12d07442d494af50af60883099909d64f032
arthy <arthy@networkgroups.in>
2025-03-18 16:19:48 +0530

    done

--- app/src/main/java/pro/network/nanjilmartdelivery/app/FirebaseMessageReceiver.java
+++ app/src/main/java/pro/network/nanjilmartdelivery/app/FirebaseMessageReceiver.java
@@ -7,12 +7,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.widget.RemoteViews;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
@@ -30,7 +29,7 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //handle when receive notification via data event
-        if (remoteMessage.getData().size() > 0) {
+        if (!remoteMessage.getData().isEmpty()) {
             showNotification(remoteMessage.getData().get("title"),
                     remoteMessage.getData().get("message"));
         }
@@ -43,19 +42,11 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
     }
 
-    private RemoteViews getCustomDesign(String title, String message) {
-        RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification);
-        remoteViews.setTextViewText(R.id.title, title);
-        remoteViews.setTextViewText(R.id.message, message);
-        remoteViews.setImageViewResource(R.id.icon, R.drawable.nanjilmart);
-        return remoteViews;
-    }
-
     public void showNotification(String title, String message) {
         Intent intent = new Intent(this, SplashActivity.class);
         String channel_id = "web_app_channel";
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent =  PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channel_id)
                 .setSmallIcon(R.drawable.nanjilmart)
@@ -65,13 +56,10 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
                 .setOnlyAlertOnce(false)
                 .setContentIntent(pendingIntent);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            builder = builder.setContent(getCustomDesign(title, message));
-        } else {
             builder = builder.setContentTitle(title)
                     .setContentText(message)
                     .setSmallIcon(R.drawable.nanjilmart);
-        }
+
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -90,12 +78,12 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
             // mediaPlayer.setDataSource(String.valueOf(myUri));
             mediaPlayer.setDataSource(FirebaseMessageReceiver.this, sound);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
         }
         try {
             mediaPlayer.prepare();
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
         }
         mediaPlayer.start();
         notificationManager.notify(0, builder.build());
@@ -110,7 +98,7 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
                 getApplicationContext(),
                 1001,
                 new Intent(getApplicationContext(), FirebaseMessageReceiver.class),
-                PendingIntent.FLAG_ONE_SHOT);
+                PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, service);
     }
